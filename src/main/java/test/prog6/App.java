ce621ac0ef8230783fb48e93853d3c6a6555a003
Madhu98-bg <madhu17gowdru@gmail.com>
2025-04-01 14:47:44 +0530

    madhu

--- src/main/java/test/prog6/App.java
+++ src/main/java/test/prog6/App.java
@@ -17,7 +17,7 @@ public class App
     		throw new IllegalArgumentException(" Number must be positive");
     	}
     	long factorial = 1;
-    	for(int i = 1; i < number; i++) {
+    	for(int i = 1; i <=number; i++) {
     		factorial *= i;
     	}
     	return factorial;
